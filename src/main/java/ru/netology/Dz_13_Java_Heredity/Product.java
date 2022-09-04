c1843de9490aec5b3409550a3d5d0bd138ab918b
VozunMaxim <vozun.maxim@gmail.com>
2022-09-04 10:49:57 +0300

    first commit

---
+++ src/main/java/ru/netology/Dz_13_Java_Heredity/Product.java
@@ -0,0 +1,4 @@
+package ru.netology.Dz_13_Java_Heredity;
+
+public class Product {
+}
---
+++ src/test/java/ru/netology/Dz_13_Java_Heredity/ProductManagerTest.java
@@ -0,0 +1,15 @@
+package ru.netology.Dz_13_Java_Heredity;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductManagerTest {
+
+    @Test
+    void searchBy() {
+    }
+
+    @Test
+    void matches() {
+    }
+}
